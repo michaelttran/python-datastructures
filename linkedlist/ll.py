a5d6793fe40bb9be3ee504120f184a547a113272
michaeltranDT <tktran5@usfca.edu>
2018-12-24 20:17:42 -0800

    added two linked lists together from left to right

--- linkedlist/ll.py
+++ linkedlist/ll.py
@@ -121,6 +121,30 @@ class LinkedList:
 			prev = curr
 			curr = curr.next
 
+	def add_l_to_r(self, h2):
+		# self.list_print()
+		# h2.list_print()
+		num1 = ""
+		num2 = ""
+		curr = self.headval
+		curr2 = h2.headval
+		while curr is not None:
+			num1 += str(curr.val)
+			curr = curr.next
+
+		while curr2 is not None:
+			num2 += str(curr2.val)
+			curr2 = curr2.next
+
+		total = int(num1) + int(num2)
+		print("ll1: ")
+		self.list_print()
+		print("ll2: ")
+		h2.list_print()
+
+		print()
+		print("The sum of the two linked lists from left to right: ", total)
+
 
 
 
@@ -128,7 +152,10 @@ class LinkedList:
 
 
 list1 = LinkedList()
+list2 = LinkedList()
 list1.headval = Node(0)
+list2.headval = Node(0)
+
 n1 = Node(1)
 n7 = Node(1)
 n8 = Node(2)
@@ -138,6 +165,9 @@ n4 = Node(4)
 n5 = Node(4)
 n6 = Node(4)
 
+n9 = Node(1)
+n10 = Node(1)
+
 list1.add_to_end(n1)
 list1.add_to_end(n8)
 list1.add_to_end(n7)
@@ -145,26 +175,29 @@ list1.add_to_end(n2)
 list1.add_to_end(n4)
 list1.add_to_end(n5)
 list1.add_to_end(n6)
+
+list2.add_to_end(n9)
+list2.add_to_end(n10)
 # list1.add_to_end(n5)
 # list1.add_to_end(n6)
 
-print("Before")
-list1.list_print()
-print()
+# print("Before")
+# list1.list_print()
+# print()
 
 # list1.rm_dupe()
 # list1.delete(5)
 
 # list1.reverse()
-list1.rm_dupe()
+# list1.rm_dupe()
 
-list1.rm_kth_last(1)
+# list1.rm_kth_last(1)
 
-
-
-
+list1.add_l_to_r(list2)
 
 
-print()
-print("After: ")
-list1.list_print()
+
+
+# print()
+# print("After: ")
+# list1.list_print()
